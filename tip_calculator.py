0bd7f3f96d270bf3bfac285866ab9e797afc4166
bmoff7 <32845158+bmoff7@users.noreply.github.com>
2022-05-15 21:27:21 -0400

    Update tip_calculator.py

--- tip_calculator.py
+++ tip_calculator.py
@@ -10,7 +10,8 @@ tip = input('What percentage tip would you like to give? 10, 12, or 15?\n')
 split = input('How many people to split the bill?\n')
 
 
-payment = ((int(total) + (int(total)*int(tip))/100)/int(split))
-message = (f'Each person should pay: ${payment} ')
+payment = ((float(total) + (float(total)*int(tip))/100)/int(split))
+r_payment = round(payment,2)
+message = (f'Each person should pay: ${r_payment} ')
 print (message) 
        
